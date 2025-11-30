28c8e16e2468cae05b2b52e71eee3c6d61e8b48c
Yelyzaveta Dorokhova <116223638+Dorokhovay@users.noreply.github.com>
2025-11-30 20:35:57 +0200

    Delete src directory

--- src/student_jobs/word_count/mapper.py
+++
@@ -1,8 +0,0 @@
-from src.core.job.mapper import Mapper
-
-
-class WordCountMapper(Mapper):
-    def map(self, record, emit):
-        for token in str(record).split():
-            emit(token.lower(), 1)
-
